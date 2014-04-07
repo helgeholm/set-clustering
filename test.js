f402174be4a966102848be72ee6e9ec24ce7f539
Helge Skogly Holm <helge.holm@gmail.com>
2014-04-07 13:29:02 +0200

    Added manual tests why not.

---
+++ test.js
@@ -0,0 +1,23 @@
+var difflib = require('difflib');
+var cluster = require('./cluster');
+
+var data = require('./words');
+data = ['anne', 'anna', 'arne', 'bendrikk', 'bendrik', 'hendrik'];
+
+function similarity(x, y) {
+  return (new difflib.SequenceMatcher(null, x, y)).ratio();
+}
+
+/*
+var threshold = 0.4285714289;
+var graph = require('./lib/graph');
+var g = graph.create(data, similarity);
+var t = graph.threshold(g, threshold);
+var c = graph.connected(t, 100);
+console.log(threshold, c.length, c[0].nodes.length);
+*/
+
+var c = cluster(data, similarity).group(2);
+
+console.log(c);
+console.log(c.length);
