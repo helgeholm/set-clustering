6846bcfe4c622c1762015a51443158f4e7e6957b
Helge Skogly Holm <helge.holm@gmail.com>
2015-03-17 12:23:55 +0100

    Added part 2 of example 1 to test.

--- test/examples.js
+++ test/examples.js
@@ -2,24 +2,34 @@ var cluster = require('../cluster');
 var testTools = require('../test-tools');
 var assert = require('assert');
 
-var deepUnorderedEquals = testTools.deepUnorderedEquals;
-var editDistancePercentage = testTools.editDistancePercentage;
-
 describe('set-clustering behaves according to examples', function() {
-  it('names into 2 groups, based on edit distance', function() {
-    var words = [
-      'stephen',
-      'albert',
-      'stephanie',
-      'bernard',
-      'norbert'
-    ];
+  var deepUnorderedEquals = testTools.deepUnorderedEquals;
+  var editDistancePercentage = testTools.editDistancePercentage;
+  var example1words = [
+    'stephen',
+    'albert',
+    'stephanie',
+    'bernard',
+    'norbert'
+  ];
 
-    var c = cluster(words, editDistancePercentage).groups(2);
+  it('divides names into 2 groups, based on edit distance', function() {
+    var c = cluster(example1words, editDistancePercentage).groups(2);
 
     assert(deepUnorderedEquals(
       c,
       [ [ 'norbert', 'albert', 'bernard' ],
+        [ 'stephanie', 'stephen' ] ]
+    ));
+  });
+
+  it('divides names into 3 groups, based on edit distance', function() {
+    var c = cluster(example1words, editDistancePercentage).groups(3);
+
+    assert(deepUnorderedEquals(
+      c,
+      [ [ 'norbert', 'albert' ],
+        [ 'bernard' ],
         [ 'stephanie', 'stephen' ] ]
     ));
   });
